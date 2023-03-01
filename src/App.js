dd53fc40f9841d25b10bebe8d07027b3751d3c2f
Riley Chamberlain <rl636888@dal.ca>
2023-03-01 13:59:36 -0400

    added lab files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import monkey from './labmonk.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+  
+      <div className="images">
+      <img src={monkey} alt="A majestic monkey" height="200px" witdh="200px" />
+      </div>
+
+      <div className="lab#">
+      Lab6
+      </div>
+
+      <div className="PI">
+       Riley Chamberlain 
+      B00846034
+      </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
